60306fa4a3f6b3addf15e285f2fb1848216384a9
Arbinda Dangi <cihe240369@student.cihe.edu.au>
2025-08-20 09:33:42 +1000

    Delete backend/api directory

--- backend/api/register.py
+++
@@ -1,9 +0,0 @@
-{
-  "name": "Arbinda Dangi",
-  "username": "arbinda",
-  "email": "cihe240369@student.cihe.edu.au",
-  "password": "Passw0rd123!",
-  "student_id": "CIHE240369",
-  "phone": "0400 000 000",
-  "address": "Canberra"
-}
