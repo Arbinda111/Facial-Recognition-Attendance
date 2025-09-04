210247a298455a9ccdf3e357cce8fc5b6ddcd8b3
sudip090 <cihe23670@student.cihe.edu.au>
2025-09-05 00:21:44 +1000

    updated configuration

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'newpassword');
 define('DB_NAME', 'face_recog');
 
 // Create database connection
